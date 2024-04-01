0552ff5b72244d03d0c56ae443370e41dc00efa1
luna <bro.wahyeung@gmail.com>
2024-04-01 18:18:46 -0400

    Commit message

---
+++ src/main/java/io/getarrays/server/model/Server.java
@@ -0,0 +1,36 @@
+package io.getarrays.server.model;
+
+import io.getarrays.server.enumeration.Status;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.AUTO;
+
+/**
+ * @author Get Arrays (https://www.getarrays.io/)
+ * @version 1.0
+ * @since 9/4/2021
+ */
+
+@Entity
+@Data
+@NoArgsConstructor@AllArgsConstructor
+public class Server {
+        @Id
+        @GeneratedValue(strategy = AUTO)
+        private Long id;
+        @Column(unique = true)
+        @NotEmpty(message = "IP Address cannot be empty or null")
+        private String ipAddress;
+        private String name;
+        private String memory;
+        private String type;
+        private String imageUrl;
+        private Status status;
+}
